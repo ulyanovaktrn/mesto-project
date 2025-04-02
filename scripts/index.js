8d60244838f07562ea5fc85bcace3f86185ae6d1
ulyanovaktrn <ulyanova24ktrn@gmail.com>
2025-04-02 23:28:54 +0300

    feat: add funcs for cards and modals (pt.1,2)

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,40 @@
-// @todo: Темплейт карточки
+const profilePopup = document.querySelector('.popup_type_edit');
+const cardPopup = document.querySelector('.popup_type_new-card');
+const imagePopup = document.querySelector('.popup_type_image');
+
+//Темплейт карточки
+const cardTemplate = document.querySelector('#card-template');
+const placesList = document.querySelector(".places__list");
 
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
+//Функция создания карточки
+function createCard(itemCard) {
+  const cloneCardTemplate = cardTemplate.content.cloneNode(true);
+  const imageCard = cloneCardTemplate.querySelector('.card__image');
+  const titleCard = cloneCardTemplate.querySelector('.card__title');
 
-// @todo: Функция удаления карточки
+  imageCard.src = itemCard.link;
+  imageCard.alt = itemCard.name;
+  titleCard.textContent = itemCard.name;
 
-// @todo: Вывести карточки на страницу
+  return itemCard;
+}
+
+//Функция удаления карточки
+
+
+//Вывести карточки на страницу
+initialCards.forEach((item) => {
+  //console.log(createCard(item));
+  placesList.append(createCard(item));
+});
+
+
+function openModal(popup){
+  popup.classList.add('popup_is-opened');
+}
+
+function closeModal(popup) {
+  popup.classList.remove('popup_is-opened');
+}
