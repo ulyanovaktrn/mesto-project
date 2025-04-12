87f405679fe7e04d057295321d95e66359ed49d9
ulyanovaktrn <ulyanova24ktrn@gmail.com>
2025-04-12 12:04:56 +0300

    feat: add close by esc (pt.4)

--- scripts/components/index.js
+++ scripts/components/index.js
@@ -39,13 +39,21 @@ initialCards.forEach((item) => {
   placesList.append(createCard(item));
 });
 
+function closeByEsc(evt) {
+  if (evt.key === "Escape") {
+    const openedPopup = document.querySelector('.popup_is-opened');
+    closeModal(openedPopup);
+  } 
+}
 
 function openModal(popup){
   popup.classList.add('popup_is-opened');
+  document.addEventListener('keydown', closeByEsc);
 }
 
 function closeModal(popup) {
   popup.classList.remove('popup_is-opened');
+  document.removeEventListener('keydown', closeByEsc);
 }
 
 //Функция создания карточки
